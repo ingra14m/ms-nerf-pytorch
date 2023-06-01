caa3195172422f588ee0ada1a0d9febbbc936c84
ingra14m <1261238025@qq.com>
2023-06-01 19:17:13 +0800

    Use relu instead of sigmoid to activate feature

--- run_nerf.py
+++ run_nerf.py
@@ -279,7 +279,7 @@ def raw2outputs(raw, z_vals, rays_d, raw_noise_std=0, white_bkgd=False, pytest=F
 
     dists = dists * torch.norm(rays_d[..., None, :], dim=-1)
 
-    feature = torch.sigmoid(raw[..., :K * d]) if K == 1 else torch.sigmoid(raw[...,:3])   # [N_rays, N_samples, 3]
+    feature = torch.relu(raw[..., :K * d]) if K > 1 else torch.sigmoid(raw[...,:3])   # [N_rays, N_samples, 3]
     batch, sample = feature.shape[:2]
     noise = 0.
     if raw_noise_std > 0.:
